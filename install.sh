1c251dae191e8f06498f83272419214f3f5117c6
Junior Hsu <ayijun@gmail.com>
2015-10-28 12:51:50 +0800

    fixes Couldn't find any package by regex 'ruby2.1'

--- install.sh
+++ install.sh
@@ -3,8 +3,14 @@
 # exit if a command fails
 set -e
 
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 80F70E11F0F0D5F10CB20E62F5DA5F09C3173AA6
+echo "deb http://ppa.launchpad.net/brightbox/ruby-ng/ubuntu trusty main" >> /etc/apt/sources.list
+
 # install ruby 2.1
-apt-get update && apt-get install -y ruby2.1
+DEBIAN_FRONTEND=noninteractive apt-get update && apt-get install -y ruby2.1
+update-locale LANG=C.UTF-8 LC_MESSAGES=POSIX \
+ && locale-gen en_US.UTF-8 \
+ && dpkg-reconfigure locales \
 
 # install mailcatcher
 gem install --no-document mailcatcher
